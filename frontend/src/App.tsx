9ea89630de520f721d4e5ec30d0ef0bdcd374691
Parthiv Ghosh <g.parthiv119@gmail.com>
2026-08-17 11:07:37 +0000

    feat: implement ProtectedRoute for dashboard access control

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,30 @@
 import NavBar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+  function ProtectedRoute({ children }: { children: React.ReactNode }) {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      return <Navigate to="/login" replace />;
+    }
+
+    return children;
+  }
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-white dark:bg-neutral-900">
         <NavBar />
 
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/" element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>} />
+          <Route path="/login" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
     </BrowserRouter>
