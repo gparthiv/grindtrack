d42d09dfdd5c2e009e38ebf95c43fb611c700d55
Parthiv Ghosh <g.parthiv119@gmail.com>
2026-08-17 07:26:40 +0000

    chore: prepare app for deployment

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,12 @@ app.use(
   taskRoute
 );
 
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "ok",
+  });
+});
+
 connectMongoDb(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((error) =>
